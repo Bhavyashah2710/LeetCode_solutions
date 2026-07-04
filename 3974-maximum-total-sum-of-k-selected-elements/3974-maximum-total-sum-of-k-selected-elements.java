5902217ac94ac57812dd07d563e0c8b2a538f756
Bhavyashah2710 <mr.shah27271001@gmail.com>
2026-07-04 08:59:01 +0530

    Time: 48 ms (99.58%), Space: 148.6 MB (95.91%) - LeetHub

---
+++ 3974-maximum-total-sum-of-k-selected-elements/3974-maximum-total-sum-of-k-selected-elements.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long maxSum(int[] nums, int k, int mul) {
+        Arrays.sort(nums);
+        long   ans = 0;
+        for(int i = nums.length-1; i>=nums.length-k;i--)
+        {
+            if(mul>0){
+                ans+=(long)nums[i]*mul;
+                mul--;
+            }
+            else{
+                ans+=nums[i];
+            }
+        }
+    return ans;
+}
+}
