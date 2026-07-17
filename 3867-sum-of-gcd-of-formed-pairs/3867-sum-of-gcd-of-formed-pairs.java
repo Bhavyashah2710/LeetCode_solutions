fa9854198f6707183558b78dcb234a1b77097ff5
Bhavyashah2710 <mr.shah27271001@gmail.com>
2026-07-17 08:56:28 +0530

    Time: 52 ms (99.5%), Space: 108.2 MB (47%) - LeetHub

--- 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.java
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.java
@@ -1,31 +1,32 @@
-import java.util.*;
 class Solution {
-    private int gcd(int a, int b) {
+   
+    public long gcdSum(int[] arr) {
+        int[] prefi = new int[arr.length];
+        int mx = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > mx) {
+                mx = arr[i];
+            }
+            prefi[i] = gcd(mx, arr[i]);
+        }
+        Arrays.sort(prefi);
+        int i = 0;
+        int j = arr.length - 1;
+        long sum = 0;
+        while (i < j) {
+            sum += gcd(prefi[i], prefi[j]);
+            i++;
+            j--;
+        }
+        return sum;
+    }
+
+     public int gcd(int a, int b) {
         while (b != 0) {
             int temp = b;
             b = a % b;
             a = temp;
         }
         return a;
-    }
-    public long gcdSum(int[] nums) {
-        int n = nums.length;
-        int[] pgcd = new int[n];
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > max)   max = nums[i];
-            pgcd[i] = gcd(nums[i], max);
-        }
-        Arrays.sort(pgcd);
-        long vb = 0;
-        int  b = 0;
-        int v = n - 1;
-
-        while (b < v) {
-            vb += gcd(pgcd[b], pgcd[v]);
-            b++;
-            v--;
-        }
-        return vb;
     }
 }
