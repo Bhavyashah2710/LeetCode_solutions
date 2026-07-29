344be33d620bca2f38fcc2a145aac6c66ee07ce5
Bhavyashah2710 <mr.shah27271001@gmail.com>
2026-07-29 21:50:07 +0530

    Time: 23 ms (82.4%), Space: 48.2 MB (57.6%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -1,18 +1,22 @@
 class Solution {
     public String smallestPalindrome(String s) {
-        int f[] = new int[26] ;
-        for(char c : s.toCharArray()) f[c-'a']++;
+        int f[] = new int[26];
+        for (char c : s.toCharArray())     f[c - 'a']++;
         int n = s.length();
         char[] v = new char[n];
-        int l = 0 , r = n-1 ;
-        for(int i = 0 ; i < 26 ; i++){
-            while(f[i] >= 2){
-                v[l++] = (char) ('a'+i);
-                v[r--] = (char) ('a'+i);
+        int l = 0;
+        int r = n - 1;
+        for (int i = 0; i < 26; i++) {
+            char ch = (char) ('a' + i);
+            while (f[i] >= 2) {
+                v[l++] = ch;
+                v[r--] = ch;
                 f[i] -= 2;
             }
-            if(f[i]==1) v[n/2] = (char) ('a'+i);
+            if (f[i] == 1) {
+                v[n / 2] = ch;
+            }
         }
-        return new String(v);        
+        return new String(v);
     }
 }
