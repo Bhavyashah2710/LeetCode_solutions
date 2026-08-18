8eec9f5e588a820eaaf0215337bbd7acbe452252
Bhavyashah2710 <mr.shah27271001@gmail.com>
2026-08-18 10:33:00 +0530

    Time: 14 ms (39.02%), Space: 47.8 MB (75.1%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -3,12 +3,15 @@ class Solution {
         int i = 1 ;
         int j = 0;
         int v = 1 ;
-        for(int n : p){
-            j = Math.max(j,n);
-        }
+        for(int n : p)   j = Math.max(j,n);
         while(i<=j){
             int m = i + (j-i)/2 ;
-            if(fun(p,h,m)){
+            long count = 0 ;
+            for(int n : p){
+                count += n/m ;
+                if(n%m != 0) count++;
+            }
+            if(count <= h){
                 v = m ;
                 j = m-1 ;
             }
@@ -16,12 +19,5 @@ class Solution {
         }
         return v ;        
     }
-    public boolean fun(int[] p , int h , int m){
-        long count = 0 ;
-        for(int n : p){
-            count += n/m ;
-            if(n%m != 0) count++;
-        }
-        return count <= h;
-    }
+    
 }
